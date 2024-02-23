30acdd47b2a1ff593befa64306112179ba8074de
Fako Berkers <email@fakoberkers.nl>
2024-02-23 17:31:56 +0100

    Exposes OpenaiPromptResource in the admin

--- aiscope/core/admin.py
+++ aiscope/core/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from datagrowth.resources.admin import HttpResourceAdmin
 
-from core.models import OpenaiEmbeddingsResource
+from core.models import OpenaiEmbeddingsResource, OpenaiPromptResource
 
 
 admin.site.register(OpenaiEmbeddingsResource, HttpResourceAdmin)
+admin.site.register(OpenaiPromptResource, HttpResourceAdmin)
